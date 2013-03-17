7944a81527bc45b9c1fb740b1611b139d598733a
Sallon <sallon88@gmail.com>
2013-03-17 21:41:54 +0800

    complete two dimensions

--- game.rb
+++ game.rb
@@ -5,9 +5,9 @@ class Game
   end
 
   def tick
-    @init_grid.length.times do |i|
-      if (alive?(i) && neighbours_alive?(i))
-        set_alive(i)
+    @y_length.times do |y|
+      @x_length.times do |x|
+        set_alive(x,y) if should_live?(x,y)
       end
     end
     @next_grid
@@ -15,26 +15,59 @@ class Game
 
   private 
   def build_next_grid
-    Array.new(@init_grid.length) {[0]}
+    @y_length = @init_grid.length
+    @x_length = @init_grid[0].length
+    Array.new(@y_length) {[0] * @x_length}
   end
 
-  def alive?(i)
-    @init_grid[i]  == [1]
+  def set_alive(x,y)
+    @next_grid[y][x] = 1
   end
 
-  def set_alive(i)
-    @next_grid[i] = [1]
+  def should_live?(x,y)
+    alive_neighbours_count = alive_neighbours_count(x,y)
+    if alive?(x,y)
+      case
+      when (0..1).include?(alive_neighbours_count)
+        false
+      when (2..3).include?(alive_neighbours_count)
+        true
+      when (4..8).include?(alive_neighbours_count)
+        false
+      end
+    elsif alive_neighbours_count == 3
+      true
+    else
+      false
+    end
   end
 
-  def neighbours_alive?(i)
-    up_neighbour_alive?(i) && down_neighour_alive?(i)
+  def alive_neighbours_count(x,y)
+    neighbours = 
+      [[x-1,y-1], [x,y-1], [x+1,y-1],
+       [x-1,y],            [x+1,y],
+       [x-1,y+1], [x,y+1], [x+1,y+1]]
+
+    alive_count = 0
+    neighbours.each do |neighbour|
+      if (alive?(neighbour[0], neighbour[1]))
+        alive_count += 1
+      end
+    end
+    alive_count
   end
 
-  def up_neighbour_alive?(i)
-    i < (@init_grid.length - 1) && alive?(i+1)
-  end
-  
-  def down_neighour_alive?(i)
-    (i > 0) && alive?(i-1) 
+  def alive?(x,y)
+    return false if (x > @x_length - 1 || x < 0)
+    return false if (y > @y_length - 1 || y < 0)
+    @init_grid[y][x] == 1
   end
 end
+=begin
+[
+[0,0,0,0],
+[0,0,0,0],
+[0,0,0,0],
+[0,0,0,0],
+]
+=end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,26 +4,20 @@ def assert_grid(init_grid, expected_grid)
   game = Game.new init_grid
   game.tick.should == expected_grid
 end
+
 describe Game do
   context  'y demension' do
     it '1 cell' do
-      assert_grid [[0]], [[0]]
       assert_grid [[1]], [[0]]
     end
 
     it '2 cells' do
-      assert_grid [[0], [0]],
-                  [[0], [0]]
       assert_grid [[1], [0]],
                   [[0], [0]]
     end
 
     it '3 cells ' do 
-      assert_grid [[0], [0], [0]],
-                  [[0], [0], [0]]
       assert_grid [[1], [0], [0]],
-                  [[0], [0], [0]]
-      assert_grid [[1], [1], [0]],
                   [[0], [0], [0]]
       assert_grid [[1], [1], [1]],
                   [[0], [1], [0]]
@@ -31,14 +25,47 @@ describe Game do
     end
 
     it '4 cells' do
-      assert_grid [[0], [0], [0], [0]],
-                  [[0], [0], [0], [0]]
-      assert_grid [[1], [1], [1], [0]],
-                  [[0], [1], [0], [0]]
       assert_grid [[0], [1], [1], [1]],
                   [[0], [0], [1], [0]]
       assert_grid [[1], [1], [1], [1]],
                   [[0], [1], [1], [0]]
+    end
+  end
+
+  context 'x demension' do
+    it '1 cell' do
+      assert_grid [[0]], [[0]]
+    end
+
+    it '2 cells' do
+      assert_grid [[0, 0]], [[0, 0]]
+    end
+
+    it '3 cells' do
+      assert_grid [[1,1,1]], [[0,1,0]]
+    end
+
+    it '4 cells' do
+      assert_grid [[1,1,1,1]], [[0,1,1,0]]
+      assert_grid [[1,1,1,1,0,1,0,1,1,1]],
+                  [[0,1,1,0,0,0,0,0,1,0]]
+    end
+  end
+
+  context 'x, y demension' do
+    it '2x2 cells' do
+      assert_grid [[0,0], [0,0]], [[0,0], [0,0]]
+    end
+
+    it '3x3 cells' do
+      assert_grid [[1,1,1], [0,0,0],[1,1,1]],
+                  [[0,1,0], [0,0,0],[0,1,0]]
+      assert_grid [[1,1,1],
+                   [1,1,1],
+                   [1,1,1]],
+                  [[1,0,1],
+                   [0,0,0],
+                   [1,0,1]]
     end
   end
 end
